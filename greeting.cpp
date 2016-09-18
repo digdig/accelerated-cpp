027a798cb16aa5b0707c2a17ec5e5510d3e5d690
digdig <hiluming@gmail.com>
2016-09-18 20:39:49 +0800

    add variable paddings for greeting!

--- greeting.cpp
+++ greeting.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
+int main()  {
     std::cout << "Please enter your first name: ";
     std::string name;
     std::cin >> name;
-    const std::string greetings = "Hello, " + name + "!";
+    const std::string greeting = "Hello, " + name + "!";
+    const int pad = 3;
+    const int rows = 2*pad + 3;
+    const std::string::size_type cols = 2*pad + 2 + greeting.size();
+    
+    std::cout << '\n';
 
-    const std::string spaces(greetings.size(),' ');
-    const std::string line2 = "* " + spaces + " *";
-    const std::string line3 = "* " + greetings + " *";
-    const std::string line1(line2.size(),'*');
+    for(int r = 0; r != rows; ++r)   {
+        int c = 0;
+        while(c!=cols)  {
+            if(c == pad+1 && r == pad+1) {
+                std::cout << greeting;
+                c += greeting.size();
+            }
+            else    {
+                if(r == 0 || r == rows-1 || c == 0 || c == cols-1)  {
+                    std::cout << '*';
+                }
+                else    {
+                    std::cout << ' ';
+                }
+                ++c;
+            }
+        }
+    std::cout << '\n';
+    }
 
-    std::cout << line1 << std::endl;
-    std::cout << line2 << std::endl;
-    std::cout << line3 << std::endl;
-    std::cout << line2 << std::endl;
-    std::cout << line1 << std::endl;
+    std::cout << std::endl;
+
     return 0;
+
 }
