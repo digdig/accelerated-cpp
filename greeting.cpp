b2e5155381b2d077df26e743c16c0ddbe2842228
digdig <hiluming@gmail.com>
2016-09-18 20:47:29 +0800

    add using command to shorten the input

--- greeting.cpp
+++ greeting.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
 #include <string>
-
+using std::cout;    using std::cin; 
+using std::endl;    using std::string;
 int main()  {
-    std::cout << "Please enter your first name: ";
-    std::string name;
-    std::cin >> name;
-    const std::string greeting = "Hello, " + name + "!";
+    cout << "Please enter your first name: ";
+    string name;
+    cin >> name;
+    const string greeting = "Hello, " + name + "!";
     const int pad = 3;
     const int rows = 2*pad + 3;
-    const std::string::size_type cols = 2*pad + 2 + greeting.size();
+    const string::size_type cols = 2*pad + 2 + greeting.size();
     
     std::cout << '\n';
 
     for(int r = 0; r != rows; ++r)   {
         int c = 0;
         while(c!=cols)  {
+            //when r = 0, we are writing the first row.
             if(c == pad+1 && r == pad+1) {
-                std::cout << greeting;
+                cout << greeting;
                 c += greeting.size();
             }
             else    {
                 if(r == 0 || r == rows-1 || c == 0 || c == cols-1)  {
-                    std::cout << '*';
+                    cout << '*';
                 }
                 else    {
-                    std::cout << ' ';
+                    cout << ' ';
                 }
                 ++c;
             }
         }
-    std::cout << '\n';
+        cout << '\n';
     }
 
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 
